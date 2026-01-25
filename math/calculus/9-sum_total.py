7df326353ab69bb337a1d2a10f7706ecbe97ad08
TahminaMammad <tahminamammad05@gmail.com>
2026-01-25 11:59:58 +0400

    Task9: Added docstrings

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
+"""
+Module 9-sum_total
+Provides a function to compute the sum of squares from 1 to n.
+"""
+
+
 def summation_i_squared(n):
+    """
+    Calculates the sum of squares from 1 to n.
+
+    Args:
+        n (int): stopping condition
+
+    Returns:
+        int: sum of i squared from 1 to n
+        None: if n is not a valid positive integer
+    """
     if not isinstance(n, int) or n < 1:
         return None
 
