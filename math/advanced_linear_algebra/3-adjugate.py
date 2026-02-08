e9e31290a5489431eb01703872be9ba7c9b7db14
TahminaMammad <tahminamammad05@gmail.com>
2026-02-08 10:36:08 +0000

    Task 3

---
+++ math/advanced_linear_algebra/3-adjugate.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+"""
+Module that calculates the adjugate matrix of a given matrix
+"""
+
+
+def determinant(matrix):
+    """
+    Calculates the determinant of a matrix
+
+    Args:
+        matrix (list of lists): square matrix
+
+    Returns:
+        determinant of the matrix
+    """
+    if matrix == [[]]:
+        return 1
+
+    if len(matrix) == 1:
+        return matrix[0][0]
+
+    det = 0
+    for col in range(len(matrix)):
+        sub_matrix = []
+        for row in matrix[1:]:
+            sub_matrix.append(row[:col] + row[col + 1:])
+        det += ((-1) ** col) * matrix[0][col] * determinant(sub_matrix)
+
+    return det
+
+
+def minor(matrix):
+    """
+    Calculates the minor matrix of a matrix
+
+    Args:
+        matrix (list of lists): matrix to compute the minor of
+
+    Returns:
+        list of lists: minor matrix
+    """
+    if len(matrix) == 1:
+        return [[1]]
+
+    size = len(matrix)
+    minor_matrix = []
+
+    for i in range(size):
+        row_minor = []
+        for j in range(size):
+            sub_matrix = []
+            for r in range(size):
+                if r != i:
+                    sub_matrix.append(
+                        matrix[r][:j] + matrix[r][j + 1:]
+                    )
+            row_minor.append(determinant(sub_matrix))
+        minor_matrix.append(row_minor)
+
+    return minor_matrix
+
+
+def cofactor(matrix):
+    """
+    Calculates the cofactor matrix of a matrix
+
+    Args:
+        matrix (list of lists): matrix to compute the cofactor of
+
+    Returns:
+        list of lists: cofactor matrix
+    """
+    if len(matrix) == 1:
+        return [[1]]
+
+    minor_matrix = minor(matrix)
+    size = len(matrix)
+    cofactor_matrix = []
+
+    for i in range(size):
+        row = []
+        for j in range(size):
+            row.append(((-1) ** (i + j)) * minor_matrix[i][j])
+        cofactor_matrix.append(row)
+
+    return cofactor_matrix
+
+
+def adjugate(matrix):
+    """
+    Calculates the adjugate matrix of a matrix
+
+    Args:
+        matrix (list of lists): matrix to compute the adjugate of
+
+    Returns:
+        list of lists: adjugate matrix
+    """
+    if not isinstance(matrix, list) or matrix == []:
+        raise TypeError("matrix must be a list of lists")
+
+    if not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+
+    if any(len(row) != len(matrix) for row in matrix):
+        raise ValueError("matrix must be a non-empty square matrix")
+
+    if len(matrix) == 1:
+        return [[1]]
+
+    cofactor_matrix = cofactor(matrix)
+    size = len(matrix)
+    adj = []
+
+    for i in range(size):
+        row = []
+        for j in range(size):
+            row.append(cofactor_matrix[j][i])
+        adj.append(row)
+
+    return adj
