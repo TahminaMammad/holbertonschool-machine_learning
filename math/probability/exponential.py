5650bde1c3559a24e6c3cf369f0c537421ca51b7
TahminaMammad <tahminamammad05@gmail.com>
2026-02-11 17:31:25 +0000

    Task 5

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -1,47 +1,32 @@
 #!/usr/bin/env python3
-"""Exponential distribution module"""
-
+"""
+Exponential distribution module
+"""
 
 class Exponential:
-    """Represents an exponential distribution"""
+    """
+    Exponential distribution class
+    """
+    e = 2.7182818285
 
     def __init__(self, data=None, lambtha=1.):
         """
-        Initializes the Exponential distribution
-
-        Args:
-            data (list): data to estimate lambtha
-            lambtha (float): expected number of occurrences
-
-        Raises:
-            TypeError: if data is not a list
-            ValueError: if data contains less than two values
-            ValueError: if lambtha is not positive
+        Initialize exponential distribution
+        data: list of data to estimate lambda
+        lambtha: rate parameter if data is not provided
         """
         if data is None:
-            if lambtha <= 0:
-                raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
         else:
-            if not isinstance(data, list):
-                raise TypeError("data must be a list")
-            if len(data) < 2:
-                raise ValueError("data must contain multiple values")
-            mean = sum(data) / len(data)
-            self.lambtha = float(1 / mean)
+            if type(data) is not list or len(data) < 2:
+                raise ValueError("data must be a list with at least two values")
+            self.lambtha = 1 / (sum(data) / len(data))
 
-    def pdf(self, x):
+    def cdf(self, x):
         """
-        Calculates the value of the PDF for a given time period
-
-        Args:
-            x (float): time period
-
-        Returns:
-            float: PDF value for x
+        Calculates the value of the CDF for a given time period x
+        Returns 0 if x is out of range
         """
         if x < 0:
             return 0
-
-        e = 2.7182818285
-        return self.lambtha * (e ** (-self.lambtha * x))
+        return 1 - Exponential.e ** (-self.lambtha * x)
