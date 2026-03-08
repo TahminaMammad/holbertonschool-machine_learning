442008babd0c6f73d88890eef985b74ce74f904c
TahminaMammad <tahminamammad05@gmail.com>
2026-03-08 15:16:40 +0000

    Task 8

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -1,32 +1,65 @@
 #!/usr/bin/env python3
-"""Module that defines a neural network with one hidden layer"""
-
+"""
+Defines a class NeuralNetwork that performs binary classification
+"""
 import numpy as np
 
 
 class NeuralNetwork:
-    """Neural network with one hidden layer performing binary classification"""
+    """
+    Represents a neural network with one hidden layer
+    """
 
     def __init__(self, nx, nodes):
-        """Initialize the neural network"""
-        # Validate nx
+        """
+        Class constructor
+        Args:
+            nx: number of input features
+            nodes: number of nodes in the hidden layer
+        """
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-
-        # Validate nodes
         if not isinstance(nodes, int):
             raise TypeError("nodes must be an integer")
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
 
-        # Hidden layer
-        self.W1 = np.random.randn(nodes, nx)
-        self.b1 = np.zeros((nodes, 1))
-        self.A1 = 0
+        # Hidden layer weights and biases
+        self.__W1 = np.random.normal(size=(nodes, nx))
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
 
-        # Output neuron
-        self.W2 = np.random.randn(1, nodes)
-        self.b2 = 0
-        self.A2 = 0
+        # Output layer weights and biases
+        self.__W2 = np.random.normal(size=(1, nodes))
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def b1(self):
+        return self.__b1
+
+    @property
+    def A1(self):
+        return self.__A1
+
+    @A1.setter
+    def A1(self, value):
+        self.__A1 = value
+
+    @property
+    def W2(self):
+        return self.__W2
+
+    @property
+    def b2(self):
+        return self.__b2
+
+    @property
+    def A2(self):
+        return self.__A2
