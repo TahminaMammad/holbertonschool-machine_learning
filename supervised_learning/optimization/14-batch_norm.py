396bd86ac39039fd2f11a7e660fbf9ff5e8ddca7
TahminaMammad <tahminamammad05@gmail.com>
2026-03-15 17:47:04 +0000

    Task 14

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -12,31 +12,32 @@ def create_batch_norm_layer(prev, n, activation):
     Args:
         prev: the activated output of the previous layer
         n: the number of nodes in the layer to be created
-        activation: the activation function to be used on the output
+        activation: the activation function that should be used on the output
                     of the layer
 
     Returns:
         A tensor of the activated output for the layer
     """
-    # 1. Initialize weights using VarianceScaling
+    # 1. Setup the Kernel Initializer
+    # mode='fan_avg' ensures weights are scaled based on input/output size
     init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
 
-    # 2. Create the base Dense layer
-    # We set use_bias=False because Batch Norm has its own beta parameter
-    # that acts as a bias.
-    model = tf.keras.layers.Dense(units=n,
-                                  kernel_initializer=init,
-                                  use_bias=False)
-    Z = model(prev)
+    # 2. Create the Dense layer
+    # IMPORTANT: use_bias=False because BatchNormalization provides 'beta'
+    # activation=None because we normalize BEFORE activating
+    dense = tf.keras.layers.Dense(
+        units=n,
+        kernel_initializer=init,
+        use_bias=False
+    )
+    Z = dense(prev)
 
     # 3. Create the Batch Normalization layer
-    # gamma_initializer='ones' and beta_initializer='zeros' are defaults
+    # gamma_init default is 'ones', beta_init default is 'zeros'
     batch_norm = tf.keras.layers.BatchNormalization(epsilon=1e-7)
-
-    # 4. Apply normalization to the unactivated output
     Z_norm = batch_norm(Z)
 
-    # 5. Apply the activation function
+    # 4. Apply activation if it exists
     if activation is None:
         return Z_norm
 
