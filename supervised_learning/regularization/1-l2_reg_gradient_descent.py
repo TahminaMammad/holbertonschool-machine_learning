c3e11b9d0e6c21e584efb2fa25b49e54bfd51ca1
TahminaMammad <tahminamammad05@gmail.com>
2026-03-22 09:56:25 +0000

    Fixed Task 1

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -7,34 +7,34 @@ import numpy as np
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """
-    Updates weights and biases using gradient descent with L2 regularization
+    Updates the weights and biases of a neural network using
+    gradient descent with L2 regularization
 
     Parameters:
     Y (numpy.ndarray): one-hot labels (classes, m)
-    weights (dict): weights and biases
-    cache (dict): activations
+    weights (dict): dictionary of weights and biases
+    cache (dict): dictionary of activations
     alpha (float): learning rate
     lambtha (float): L2 regularization parameter
     L (int): number of layers
     """
     m = Y.shape[1]
-    weights_copy = weights.copy()
 
-    # Output layer
+    # Initialize dZ for output layer
     dZ = cache['A' + str(L)] - Y
 
     for i in reversed(range(1, L + 1)):
         A_prev = cache['A' + str(i - 1)]
-        W = weights_copy['W' + str(i)]
+        W = weights['W' + str(i)]
 
-        # Gradient with L2
+        # Compute gradients with L2 regularization
         dW = (1 / m) * np.matmul(dZ, A_prev.T) + (lambtha / m) * W
         db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
 
-        # Update
+        # Compute dZ for next layer BEFORE updating weights
+        if i > 1:
+            dZ = np.matmul(W.T, dZ) * (1 - np.square(A_prev))
+
+        # Update weights and biases
         weights['W' + str(i)] -= alpha * dW
         weights['b' + str(i)] -= alpha * db
-
-        if i > 1:
-            A_prev = cache['A' + str(i - 1)]
-            dZ = np.matmul(W.T, dZ) * (1 - np.square(A_prev))
