49a21cdc4e504510554e3e4661195da0ea4b0fb2
TahminaMammad <tahminamammad05@gmail.com>
2026-03-29 14:54:24 +0000

    Task 5

---
+++ math/convolutions_and_pooling/5-convolve.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""Module for performing convolution with multiple kernels"""
+
+
+import numpy as np
+
+
+def convolve(images, kernels, padding='same', stride=(1, 1)):
+    """Performs a convolution on images using multiple kernels
+
+    Args:
+        images (numpy.ndarray): shape (m, h, w, c)
+        kernels (numpy.ndarray): shape (kh, kw, c, nc)
+        padding: 'same', 'valid', or tuple (ph, pw)
+        stride (tuple): (sh, sw)
+
+    Returns:
+        numpy.ndarray: convolved images
+    """
+    m, h, w, c = images.shape
+    kh, kw, kc, nc = kernels.shape
+    sh, sw = stride
+
+    # Determine padding
+    if padding == 'same':
+        ph = (h - 1) * sh + kh - h
+        pw = (w - 1) * sw + kw - w
+
+        ph = ph // 2 + (ph % 2)
+        pw = pw // 2 + (pw % 2)
+
+    elif padding == 'valid':
+        ph, pw = 0, 0
+
+    else:
+        ph, pw = padding
+
+    # Apply padding
+    padded = np.pad(
+        images,
+        ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+        mode='constant'
+    )
+
+    # Output dimensions
+    output_h = (h + 2 * ph - kh) // sh + 1
+    output_w = (w + 2 * pw - kw) // sw + 1
+
+    output = np.zeros((m, output_h, output_w, nc))
+
+    for i in range(output_h):
+        for j in range(output_w):
+            region = padded[
+                :,
+                i * sh:i * sh + kh,
+                j * sw:j * sw + kw,
+                :
+            ]
+            for k in range(nc):
+                output[:, i, j, k] = np.sum(
+                    region * kernels[:, :, :, k],
+                    axis=(1, 2, 3)
+                )
+
+    return output
