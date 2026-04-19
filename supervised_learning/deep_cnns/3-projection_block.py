bd1d03201db22c67ab24bda3821bbdd007cae2a1
TahminaMammad <tahminamammad05@gmail.com>
2026-04-19 07:07:54 +0000

    Task 3

---
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""
+Projection block for ResNet
+"""
+
+from tensorflow import keras as K
+
+
+def projection_block(A_prev, filters, s=2):
+    """
+    Builds a projection block as described in ResNet
+
+    Parameters:
+    A_prev -- output from previous layer
+    filters -- tuple/list of (F11, F3, F12)
+    s -- stride for the first convolution
+
+    Returns:
+    Activated output of the projection block
+    """
+
+    F11, F3, F12 = filters
+    initializer = K.initializers.he_normal(seed=0)
+
+    # Shortcut path
+    shortcut = K.layers.Conv2D(filters=F12,
+                               kernel_size=(1, 1),
+                               strides=(s, s),
+                               padding='same',
+                               kernel_initializer=initializer)(A_prev)
+    shortcut = K.layers.BatchNormalization(axis=3)(shortcut)
+
+    # Main path
+    # 1x1 conv (with stride s)
+    X = K.layers.Conv2D(filters=F11,
+                        kernel_size=(1, 1),
+                        strides=(s, s),
+                        padding='same',
+                        kernel_initializer=initializer)(A_prev)
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Activation('relu')(X)
+
+    # 3x3 conv
+    X = K.layers.Conv2D(filters=F3,
+                        kernel_size=(3, 3),
+                        padding='same',
+                        kernel_initializer=initializer)(X)
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Activation('relu')(X)
+
+    # 1x1 conv
+    X = K.layers.Conv2D(filters=F12,
+                        kernel_size=(1, 1),
+                        padding='same',
+                        kernel_initializer=initializer)(X)
+    X = K.layers.BatchNormalization(axis=3)(X)
+
+    # Add shortcut
+    X = K.layers.Add()([X, shortcut])
+    X = K.layers.Activation('relu')(X)
+
+    return X
