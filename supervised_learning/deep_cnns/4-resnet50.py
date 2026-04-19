1b82919700720edc31bcf16486aa807281210551
TahminaMammad <tahminamammad05@gmail.com>
2026-04-19 17:24:21 +0000

    Task 4

--- supervised_learning/deep_cnns/4-resnet50.py
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""
-ResNet-50 architecture
-"""
+"""ResNet-50 architecture"""
 
 from tensorflow import keras as K
 
@@ -10,27 +8,25 @@ projection_block = __import__('3-projection_block').projection_block
 
 
 def resnet50():
-    """
-    Builds the ResNet-50 architecture
-
-    Returns:
-    keras model
-    """
-
-    initializer = K.initializers.he_normal(seed=0)
+    """Builds the ResNet-50 architecture"""
+    init = K.initializers.he_normal(seed=0)
 
     X_input = K.Input(shape=(224, 224, 3))
 
-    # Initial layer
-    X = K.layers.Conv2D(64, (7, 7),
-                        strides=(2, 2),
-                        padding='same',
-                        kernel_initializer=initializer)(X_input)
+    # Stage 1
+    X = K.layers.Conv2D(
+        64, (7, 7),
+        strides=(2, 2),
+        padding='same',
+        kernel_initializer=init
+    )(X_input)
     X = K.layers.BatchNormalization(axis=3)(X)
     X = K.layers.Activation('relu')(X)
-    X = K.layers.MaxPooling2D((3, 3),
-                              strides=(2, 2),
-                              padding='same')(X)
+    X = K.layers.MaxPooling2D(
+        (3, 3),
+        strides=(2, 2),
+        padding='same'
+    )(X)
 
     # Stage 2
     X = projection_block(X, [64, 64, 256], s=1)
@@ -56,14 +52,16 @@ def resnet50():
     X = identity_block(X, [512, 512, 2048])
     X = identity_block(X, [512, 512, 2048])
 
-    # Pooling
+    # Average Pooling
     X = K.layers.AveragePooling2D((7, 7), padding='same')(X)
 
-    # Output
-    X = K.layers.Dense(1000,
-                       activation='softmax',
-                       kernel_initializer=initializer)(X)
+    # Output layer
+    X = K.layers.Dense(
+        1000,
+        activation='softmax',
+        kernel_initializer=init
+    )(X)
 
-    model = K.models.Model(inputs=X_input, outputs=X)
+    model = K.Model(inputs=X_input, outputs=X)
 
     return model
