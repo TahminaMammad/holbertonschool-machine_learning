681b5bd6ef7adcde17ec838812d5149c05ab631d
TahminaMammad <tahminamammad05@gmail.com>
2026-05-22 11:16:20 +0000

    Task 8

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python3
-"""Expectation-Maximization algorithm for a Gaussian Mixture Model."""
-
+i#!/usr/bin/env python3
+"""Expectation Maximization algorithm for a Gaussian Mixture Model."""
 import numpy as np
 initialize = __import__('4-initialize').initialize
 expectation = __import__('6-expectation').expectation
@@ -8,50 +7,58 @@ maximization = __import__('7-maximization').maximization
 
 
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
-    """Performs the EM algorithm for a GMM.
+    """Perform the expectation maximization algorithm for a GMM.
 
     Args:
-        X (numpy.ndarray): shape (n, d), dataset.
-        k (int): number of clusters.
-        iterations (int): max number of iterations.
-        tol (float): tolerance for early stopping.
-        verbose (bool): print log likelihood.
+        X: numpy.ndarray of shape (n, d) containing the data set.
+        k: positive integer containing the number of clusters.
+        iterations: positive integer, maximum number of iterations.
+        tol: non-negative float, tolerance of the log likelihood for early
+             stopping. Stops if the difference is less than or equal to tol.
+        verbose: boolean; if True, prints log likelihood every 10 iterations
+                 and after the last iteration.
 
     Returns:
-        pi, m, S, g, l or None on failure.
+        pi: numpy.ndarray of shape (k,) with priors for each cluster.
+        m: numpy.ndarray of shape (k, d) with centroid means for each cluster.
+        S: numpy.ndarray of shape (k, d, d) with covariance matrices.
+        g: numpy.ndarray of shape (k, n) with probabilities per data point.
+        l: log likelihood of the model.
+        Returns None, None, None, None, None on failure.
     """
-
-    if (not isinstance(X, np.ndarray) or X.ndim != 2):
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        return None, None, None, None, None
+    if not isinstance(k, int) or k <= 0:
+        return None, None, None, None, None
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None, None, None, None
+    if not isinstance(tol, float) or tol < 0:
+        return None, None, None, None, None
+    if not isinstance(verbose, bool):
         return None, None, None, None, None
 
-    try:
-        pi, m, S = initialize(X, k)
-    except Exception:
-        return None, None, None, None, None
+    pi, m, S = initialize(X, k)
+    l_prev = 0
 
-    if pi is None or m is None or S is None:
-        return None, None, None, None, None
-
-    prev_l = None
-
-    for i in range(iterations + 1):
-
+    for i in range(iterations):
         g, l = expectation(X, pi, m, S)
-
-        if g is None or l is None:
+        if g is None:
             return None, None, None, None, None
 
-        if verbose and (i % 10 == 0 or i == iterations):
-            print("Log Likelihood after {} iterations: {:.5f}".format(i, l))
+        if verbose and i % 10 == 0:
+            print("Log Likelihood after {} iterations: {}".format(
+                i, round(l, 5)))
 
-        if prev_l is not None and abs(l - prev_l) <= tol:
+        if i > 0 and abs(l - l_prev) <= tol:
             break
 
-        prev_l = l
-
+        l_prev = l
         pi, m, S = maximization(X, g)
 
-        if pi is None or m is None or S is None:
-            return None, None, None, None, None
+    g, l = expectation(X, pi, m, S)
+
+    if verbose:
+        print("Log Likelihood after {} iterations: {}".format(
+            i, round(l, 5)))
 
     return pi, m, S, g, l
