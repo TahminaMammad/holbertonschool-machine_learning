b5d3b5bb52af121b7134294dedd2f521940b74b2
TahminaMammad <tahminamammad05@gmail.com>
2026-08-11 20:34:42 +0000

    Task 1

---
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module for initializing a Q-table."""
+
+import numpy as np
+
+
+def q_init(env):
+    """Initialize the Q-table with zeros."""
+    return np.zeros((env.observation_space.n, env.action_space.n))
