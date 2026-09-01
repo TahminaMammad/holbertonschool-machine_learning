f6ed2d96c3553ca96befb0cc74b26dfc9e789d52
TahminaMammad <tahminamammad05@gmail.com>
2026-09-01 17:33:25 +0000

    Task 2

---
+++ reinforcement_learning/policy_gradients/train.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Train an agent using the Monte Carlo policy gradient method."""
+
+import numpy as np
+
+policy_gradient = __import__('policy_gradient').policy_gradient
+
+
+def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
+    """Train a policy-gradient agent and return each episode's score."""
+    weight = np.random.rand(env.observation_space.shape[0],
+                            env.action_space.n)
+    scores = []
+
+    for episode in range(nb_episodes):
+        state, _ = env.reset()
+        gradients = []
+        rewards = []
+
+        while True:
+            action, gradient = policy_gradient(state, weight)
+            state, reward, terminated, truncated, _ = env.step(action)
+            gradients.append(gradient)
+            rewards.append(reward)
+
+            if terminated or truncated:
+                break
+
+        score = sum(rewards)
+        scores.append(score)
+        print("Episode: {} Score: {}".format(episode, score))
+
+        discounted_reward = 0
+        for index in range(len(rewards) - 1, -1, -1):
+            discounted_reward = rewards[index] + gamma * discounted_reward
+            weight += alpha * discounted_reward * gradients[index]
+
+    return scores
